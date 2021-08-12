e33864bb9a89771bb7db9bd834b8d179b241dd97
daniel <dan137clarke@gmail.com>
2021-08-12 21:56:49 +1000

    first working shader!

--- src/Main.hx
+++ src/Main.hx
@@ -1,3 +1,13 @@
+class MyShader extends hxsl.Shader {
+    static var SRC = {
+        @:import h3d.shader.Base2d;
+
+        function fragment() {
+            pixelColor = vec4(sin(time / 2), sin(time / 3), sin(time / 5), 1.0);
+        }
+    }
+}
+
 class Main extends hxd.App {
     var worldDim : Int;
     var world : Array<Array<h2d.Anim>>;
@@ -12,7 +22,7 @@ class Main extends hxd.App {
         anim.x = x * (cellSize + 1);
         anim.y = y * (cellSize + 1);
         anim.alpha = 0;
-        world[x][y] = anim;
+        return anim;
     }
 
     function drawCreature(creature: Array<Array<Int>>, x, y) {
@@ -58,21 +68,22 @@ class Main extends hxd.App {
         var colors = [0xFF0000, 0xFFFF00, 0x00FF00, 0x00FFFF, 0x0000FF, 0xFF00FF];
         var tiles = [for (color in colors) h2d.Tile.fromColor(color, cellSize, cellSize)];
 
-        world = [for(i in 0...worldDim) [for(j in 0...worldDim) null]];
+        world = [for(i in 0...worldDim) [for(j in 0...worldDim) initCell(tiles, i, j)]];
         fateBuffer = [for(i in 0...worldDim) [for(j in 0...worldDim) 0]];
 
-        for (i in 0...worldDim) {
-            for (j in 0...worldDim) {
-                initCell(tiles, i, j);
-            }
-        }
-
-        drawCreature([[1, 0, 1], [0, 1, 1], [0, 1, 0]], 10, 10);
+        // drawCreature([[1, 0, 1], [0, 1, 1], [0, 1, 0]], 10, 10);
         // drawCreature([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]], 10, 10);
         // drawCreature([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]], 10, 15);
         // drawCreature([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]], 10, 20);
             
         frameRateLabel = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
+
+        var shader = new MyShader();
+        var tile = h2d.Tile.fromColor(0x00FF00, 100, 100);
+        var bmp = new h2d.Bitmap(tile, s2d);
+        bmp.x = 100;
+        bmp.y = 100;
+        bmp.addShader(shader);
     }
 
     override function update(dt : Float) {
