8dcb49fe4fa181ac0d07a3cb5fdf9d288fef2d04
daniel <dan137clarke@gmail.com>
2021-08-25 21:56:52 +1000

    rewrote using maps - slow

--- src/Main.hx
+++ src/Main.hx
@@ -57,10 +57,14 @@ class SineDeformShader extends hxsl.Shader {
 }
 
 typedef Point = {x: Int, y: Int};
+// typedef Cell = {alive: Int, population: Int};
 
 class Game {
     var world:Array<Array<h2d.Anim>>;
     var fateBuffer:Array<Array<Int>>;
+
+    var aliveMap:Map<Int, Map<Int, Int>>;
+    var populationMap:Map<Int, Map<Int, Array<Int>>>;
     
     var cellSize:Int;
     var updateRate:Float;
@@ -68,33 +72,32 @@ class Game {
     var s2d:h2d.Scene;
 
     public function new(s2d:h2d.Scene, worldDim:Int, startingCellPoints:Array<Point>) {
-        this.cellSize = 10;
-        this.updateRate = 6;
+        aliveMap = new Map();
+        populationMap = new Map();
+
+        cellSize = 5;
+        updateRate = 6;
         this.worldDim = worldDim;
         this.s2d = s2d;
 
         var colors = [0xFF0000, 0xFFFF00, 0x00FF00, 0x00FFFF, 0x0000FF, 0xFF00FF];
         var tiles = [for (color in colors) h2d.Tile.fromColor(color, cellSize, cellSize)];
 
-        this.world = [for(i in 0...worldDim) [for(j in 0...worldDim) initCell(tiles, i, j)]];
-        this.fateBuffer = [for(i in 0...worldDim) [for(j in 0...worldDim) 0]];
+        world = [for(i in 0...worldDim) [for(j in 0...worldDim) initCell(tiles, i, j)]];
+        fateBuffer = [for(i in 0...worldDim) [for(j in 0...worldDim) 0]];
 
         for (point in startingCellPoints) {
-            this.world[point.x][point.y].alpha = 1;
+            world[point.x][point.y].alpha = 1;
+            if (aliveMap.get(point.x) == null) {
+                aliveMap[point.x] = new Map();
+            }
+            aliveMap[point.x][point.y] = 1;
         }
     }
 
     public function update() {
-        for (i in 0...worldDim) {
-            for (j in 0...worldDim) {
-                determineFate(i, j);
-            }
-        }
-        for (i in 0...worldDim) {
-            for (j in 0...worldDim) {
-                updateCell(i, j);
-            }
-        }
+        updatePopulation();
+        updateAlive();
     }
 
     function initCell(tiles:Array<h2d.Tile>, x:Int, y:Int) {
@@ -105,95 +108,64 @@ class Game {
         return anim;
     }
 
-    function determineFate(x : Int, y : Int) {
-        var alive = Std.int(world[x][y].alpha);
-        var population = -alive;
-
-        for (i in [-1, 0, 1]) {
-            for (j in [-1, 0, 1]) {
-                if (0 <= x + i && x + i < worldDim) {
-                    if (0 <= y + j && y + j < worldDim) {
-                        population += Std.int(world[x + i][y + j].alpha);
+    function updatePopulation() {
+        populationMap.clear();
+        for (x in aliveMap.keys()) {
+            for (y in aliveMap[x].keys()) {
+                world[x][y].alpha = 0;
+                for (i in [-1, 0, 1]) {
+                    for (j in [-1, 0, 1]) {
+                        if (0 <= x + i && x + i < worldDim) {
+                            if (0 <= y + j && y + j < worldDim) {
+                                if (populationMap.get(x + i) == null) {
+                                    populationMap[x + i] = new Map();
+                                }
+                                if (populationMap[x + i].get(y + j) == null) {
+                                    populationMap[x + i][y + j] = [0, 0]; // {alive: 0, population: 0};
+                                }
+                                if (i == 0 && j == 0) {
+                                    populationMap[x + i][y + j][0] = 1;
+                                } else {
+                                    populationMap[x + i][y + j][1] += 1;
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
-
-        fateBuffer[x][y] = switch [alive, population] {
-            case [_, 3]: 1;
-            case [1, 2]: 1;
-            case _: 0;
-        }
     }
 
-    function updateCell(x : Int, y : Int) {
-        world[x][y].alpha = fateBuffer[x][y];
+    function updateAlive() {
+        aliveMap.clear();
+        for (x in populationMap.keys()) {
+            for (y in populationMap[x].keys()) {
+                var alive = switch populationMap[x][y] {
+                    case [_, 3]: true;
+                    case [1, 2]: true;
+                    case _: false;
+                }
+                if (alive) {
+                    if (aliveMap.get(x) == null) {
+                        aliveMap[x] = new Map();
+                    }
+                    aliveMap[x][y] = 1;
+                    world[x][y].alpha = 1;
+                }
+            }
+        }
     }
 }
 
 class Main extends hxd.App {
-    var worldDim : Int;
-    var world : Array<Array<h2d.Anim>>;
-    var cellSize : Int;
-    var fateBuffer : Array<Array<Int>>;
     var frameRateLabel : h2d.Text;
     var updateRate : Float;
     var elapsedTime : Float;
     var game : Game;
 
-    function initCell(tiles : Array<h2d.Tile>, x : Int, y : Int) {
-        var anim = new h2d.Anim(tiles, updateRate, s2d);
-        anim.x = x * (cellSize + 1);
-        anim.y = y * (cellSize + 1);
-        anim.alpha = 0;
-        return anim;
-    }
-
-    function drawCreature(creature: Array<Array<Int>>, x, y) {
-        for (i in 0...creature.length) {
-            for (j in 0...creature[i].length) {
-                if (creature[i][j] == 1) {
-                    world[x + i][y + j].alpha = 1;
-                }
-            }
-        }
-    }
-
-    function determineFate(x : Int, y : Int) {
-        var alive = Std.int(world[x][y].alpha);
-        var population = -alive;
-
-        for (i in [-1, 0, 1]) {
-            for (j in [-1, 0, 1]) {
-                if (0 <= x + i && x + i < worldDim) {
-                    if (0 <= y + j && y + j < worldDim) {
-                        population += Std.int(world[x + i][y + j].alpha);
-                    }
-                }
-            }
-        }
-
-        fateBuffer[x][y] = switch [alive, population] {
-            case [_, 3]: 1;
-            case [1, 2]: 1;
-            case _: 0;
-        }
-    }
-
-    function updateCell(x : Int, y : Int) {
-        world[x][y].alpha = fateBuffer[x][y];
-    }
-
     override function init() {
-        updateRate = 6;
-        worldDim = 50;
+        updateRate = 12;
         elapsedTime = 0;
-        cellSize = 10;
-        // var colors = [0xFF0000, 0xFFFF00, 0x00FF00, 0x00FFFF, 0x0000FF, 0xFF00FF];
-        // var tiles = [for (color in colors) h2d.Tile.fromColor(color, cellSize, cellSize)];
-
-        // world = [for(i in 0...worldDim) [for(j in 0...worldDim) initCell(tiles, i, j)]];
-        // fateBuffer = [for(i in 0...worldDim) [for(j in 0...worldDim) 0]];
 
         var startingCells = [
             {x:0, y:0},
@@ -202,12 +174,7 @@ class Main extends hxd.App {
             {x:2, y:1},
             {x:1, y:2},
         ];
-        game = new Game(s2d, 50, startingCells);
-
-        // drawCreature([[1, 0, 1], [0, 1, 1], [0, 1, 0]], 10, 10);
-        // drawCreature([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]], 10, 10);
-        // drawCreature([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]], 10, 15);
-        // drawCreature([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]], 10, 20);
+        game = new Game(s2d, 500, startingCells);
             
         frameRateLabel = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
 
@@ -272,16 +239,6 @@ class Main extends hxd.App {
         if (elapsedTime >= 1.0 / updateRate) {
             elapsedTime = 0;
             game.update();
-        //     for (i in 0...worldDim) {
-        //         for (j in 0...worldDim) {
-        //             determineFate(i, j);
-        //         }
-        //     }
-        //     for (i in 0...worldDim) {
-        //         for (j in 0...worldDim) {
-        //             updateCell(i, j);
-        //         }
-        //     }
         }
     }
 
