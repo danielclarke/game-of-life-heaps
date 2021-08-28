d492366efa2524005d2469b5f9cf2976306304ce
daniel <dan137clarke@gmail.com>
2021-08-28 16:06:10 +1000

    reusing cells instead of populating the whol map, running a glider at 60 fps

--- src/Main.hx
+++ src/Main.hx
@@ -1,4 +1,4 @@
-import format.abc.Data.ABCData;
+import Game;
 
 class MyShader extends hxsl.Shader {
     static var SRC = {
@@ -56,107 +56,6 @@ class SineDeformShader extends hxsl.Shader {
     }
 }
 
-typedef Point = {x: Int, y: Int};
-// typedef Cell = {alive: Int, population: Int};
-
-class Game {
-    var world:Array<Array<h2d.Anim>>;
-    var fateBuffer:Array<Array<Int>>;
-
-    var aliveMap:Map<Int, Map<Int, Int>>;
-    var populationMap:Map<Int, Map<Int, Array<Int>>>;
-    
-    var cellSize:Int;
-    var updateRate:Float;
-    var worldDim:Int;
-    var s2d:h2d.Scene;
-
-    public function new(s2d:h2d.Scene, worldDim:Int, startingCellPoints:Array<Point>) {
-        aliveMap = new Map();
-        populationMap = new Map();
-
-        cellSize = 5;
-        updateRate = 6;
-        this.worldDim = worldDim;
-        this.s2d = s2d;
-
-        var colors = [0xFF0000, 0xFFFF00, 0x00FF00, 0x00FFFF, 0x0000FF, 0xFF00FF];
-        var tiles = [for (color in colors) h2d.Tile.fromColor(color, cellSize, cellSize)];
-
-        world = [for(i in 0...worldDim) [for(j in 0...worldDim) initCell(tiles, i, j)]];
-        fateBuffer = [for(i in 0...worldDim) [for(j in 0...worldDim) 0]];
-
-        for (point in startingCellPoints) {
-            world[point.x][point.y].alpha = 1;
-            if (aliveMap.get(point.x) == null) {
-                aliveMap[point.x] = new Map();
-            }
-            aliveMap[point.x][point.y] = 1;
-        }
-    }
-
-    public function update() {
-        updatePopulation();
-        updateAlive();
-    }
-
-    function initCell(tiles:Array<h2d.Tile>, x:Int, y:Int) {
-        var anim = new h2d.Anim(tiles, updateRate, s2d);
-        anim.x = x * (cellSize + 1);
-        anim.y = y * (cellSize + 1);
-        anim.alpha = 0;
-        return anim;
-    }
-
-    function updatePopulation() {
-        populationMap.clear();
-        for (x in aliveMap.keys()) {
-            for (y in aliveMap[x].keys()) {
-                world[x][y].alpha = 0;
-                for (i in [-1, 0, 1]) {
-                    for (j in [-1, 0, 1]) {
-                        if (0 <= x + i && x + i < worldDim) {
-                            if (0 <= y + j && y + j < worldDim) {
-                                if (populationMap.get(x + i) == null) {
-                                    populationMap[x + i] = new Map();
-                                }
-                                if (populationMap[x + i].get(y + j) == null) {
-                                    populationMap[x + i][y + j] = [0, 0]; // {alive: 0, population: 0};
-                                }
-                                if (i == 0 && j == 0) {
-                                    populationMap[x + i][y + j][0] = 1;
-                                } else {
-                                    populationMap[x + i][y + j][1] += 1;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    function updateAlive() {
-        aliveMap.clear();
-        for (x in populationMap.keys()) {
-            for (y in populationMap[x].keys()) {
-                var alive = switch populationMap[x][y] {
-                    case [_, 3]: true;
-                    case [1, 2]: true;
-                    case _: false;
-                }
-                if (alive) {
-                    if (aliveMap.get(x) == null) {
-                        aliveMap[x] = new Map();
-                    }
-                    aliveMap[x][y] = 1;
-                    world[x][y].alpha = 1;
-                }
-            }
-        }
-    }
-}
-
 class Main extends hxd.App {
     var frameRateLabel : h2d.Text;
     var updateRate : Float;
@@ -164,7 +63,7 @@ class Main extends hxd.App {
     var game : Game;
 
     override function init() {
-        updateRate = 12;
+        updateRate = 24;
         elapsedTime = 0;
 
         var startingCells = [
